413c92de85dc7468ea64cba0803997b8bc41576b
Billy Morrison <billy.morrison1234@hotmail.co.uk>
2020-10-15 17:13:53 +0100

    removed done from tests

--- tests/artists.test.js
+++ tests/artists.test.js
@@ -21,7 +21,7 @@ describe('/artists', () => {
     })
 
     describe('POST /artists', async () => {
-        it('Creates a new artist in the database', async (done) => {
+        it('Creates a new artist in the database', async () => {
             const response = await request(app).post('/artists').send({
                 name: 'Tame Impala',
                 genre: 'Rock'
@@ -32,7 +32,6 @@ describe('/artists', () => {
             const insertedArtistRecords = await Artist.findByPk(response.body.id, { raw: true });
             expect(insertedArtistRecords.name).to.equal('Tame Impala');
             expect(insertedArtistRecords.genre).to.equal('Rock');
-            done()
         })
     })
 })
