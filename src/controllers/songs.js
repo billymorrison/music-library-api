0c8143fe411c696c7e8ce73b5f7705dfa42f3fb2
Billy Morrison <billy.morrison1234@hotmail.co.uk>
2021-05-27 07:27:17 +0100

    moving computers commit

--- src/controllers/songs.js
+++ src/controllers/songs.js
@@ -19,38 +19,3 @@ exports.create = (req, res) => {
     })
 };
 
-exports.list = (req, res) => {
-    Album.findAll({
-        where: {
-            artistId: req.params.id
-        }
-    }).then(albums => res.status(200).json(albums))
-};
-
-exports.find = (req, res) => {
-    Album.findByPk(req.params.id).then(album => album ? res.status(200).json(album) : res.status(404).json({ error: 'the album could not be found.'}))
-};
-
-exports.update = (req, res) => {
-    const { id } = req.params
-    Album.findByPk(id).then(album => {
-        if (album) {
-            Album.update(req.body, { where: { id }})
-            .then(album => res.status(200).json(album))
-        } else {
-            res.status(404).json({ error: 'the album could not be found.'})
-        }
-    })
-}
-
-exports.delete = (req, res) => {
-    const { id } = req.params
-    Album.findByPk(id).then(album => {
-        if (album) {
-            Album.destroy({ where: { id }})
-            .then(album => res.status(204).json(album))
-        } else {
-            res.status(404).json({ error: 'the album could not be found.'})
-        }
-    })
-}
